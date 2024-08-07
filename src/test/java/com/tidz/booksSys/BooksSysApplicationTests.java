937cd3acfb3eff51442a95d46733756e43d7f5eb
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-07 15:15:09 -0300

    first commit

---
+++ src/test/java/com/tidz/booksSys/BooksSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tidz.booksSys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
