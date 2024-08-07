f2e1d0df69e17459bb0e33b1bd0ad9b153bdb7ed
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-07 15:29:01 -0300

    implement getAllBooks method in BookDAOImpl

--- src/main/java/com/tidz/booksSys/repository/BookDAOImpl.java
+++ src/main/java/com/tidz/booksSys/repository/BookDAOImpl.java
@@ -3,19 +3,24 @@ package com.tidz.booksSys.repository;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.tidz.booksSys.entity.Book;
 
+import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 
 @Repository
 @Transactional
 public class BookDAOImpl implements BookDAO {
 
+	@Autowired
+	private EntityManager entityManager;
+
 	@Override
 	public List<Book> getAllBooks() {
-		return null;
+		return this.entityManager.createQuery("SELECT b FROM Book b", Book.class).getResultList();
 	}
 
 	@Override
