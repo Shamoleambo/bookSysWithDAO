1b451fbb025176764ca145bae5c232bfea825d12
Tiago Diniz Gomes <tidgomes@gmail.com>
2024-08-07 15:47:15 -0300

    implement editBook route to BookController

--- src/main/java/com/tidz/booksSys/controller/BookController.java
+++ src/main/java/com/tidz/booksSys/controller/BookController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.tidz.booksSys.entity.Book;
 import com.tidz.booksSys.service.BookService;
@@ -30,5 +33,18 @@ public class BookController {
 		Book book = new Book();
 		model.addAttribute("book", book);
 		return "new_book";
+	}
+
+	@PostMapping
+	public String saveBook(@ModelAttribute Book book) {
+		this.bookService.saveBook(book);
+		return "redirect:/books";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String editBook(@RequestParam("id") Long id, Model model) {
+		Book book = this.bookService.getBookById(id).orElseThrow(() -> new IllegalArgumentException());
+		model.addAttribute("book", book);
+		return "edit_book";
 	}
 }
